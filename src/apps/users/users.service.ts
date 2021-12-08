28affdb8c48bbc6e5c556247a917459597915b4b
temujins <mdsifatulislam.rabbi@gmail.com>
2021-12-08 12:34:38 +0600

    feat(): update user service

--- src/apps/users/users.service.ts
+++ src/apps/users/users.service.ts
@@ -1,6 +1,6 @@
 import { IUser, IUserDocument, IUserPreview, IUsersModel } from "../../typings";
 import { usersModel } from "./users.model";
-import { Passwords } from "../../libs";
+import { Passwords } from "../../common";
 
 class UsersService {
   constructor(private readonly usersModel: IUsersModel) {}
@@ -9,50 +9,33 @@ class UsersService {
     return {
       _id: user._id,
       email: user.email,
-      username: user.username,
-      full_name: user.full_name,
+      name: user.name,
       shop_id: user.shop_id,
       shop_name: user.shop_name,
     };
   }
 
   public async findAll(): Promise<IUserDocument[]> {
-    const users: IUserDocument[] = await this.usersModel.find(
-      {},
-      "_id username email full_name shop_id shop_name",
-    );
+    const users: IUserDocument[] = await this.usersModel.find({});
 
     return users;
   }
 
   public async findOne(userInfo: { _id: string }): Promise<IUserDocument>;
-  public async findOne(userInfo: { username: string }): Promise<IUserDocument>;
   public async findOne(userInfo: { email: string }): Promise<IUserDocument>;
-  public async findOne(userInfo: {
-    _id: string;
-    email: string;
-    username: string;
-  }): Promise<IUserDocument | null> {
+  public async findOne(userInfo: { _id: string; email: string }): Promise<IUserDocument | null> {
     const user: IUserDocument | null = await this.usersModel.findOne(userInfo);
     return user;
   }
 
   public async create(data: IUser): Promise<IUserDocument> {
-    if (
-      (await this.findOne({ email: data.email })) ||
-      (await this.findOne({ username: data.username }))
-    ) {
-      throw "Username or Email already in use";
+    if (await this.findOne({ email: data.email })) {
+      throw "Email already in use";
     }
 
     const hash: string = await Passwords.genPass(data.password);
 
-    const newUser: IUserDocument = new this.usersModel({
-      email: data.email,
-      full_name: data.full_name,
-      username: data.username,
-      password: hash,
-    });
+    const newUser: IUserDocument = new this.usersModel({ ...data, password: hash });
 
     const createdUser: IUserDocument = await newUser.save();
     return createdUser;
@@ -62,9 +45,8 @@ class UsersService {
     const user: IUserDocument = await this.findOne({ _id: userId });
     const updateData: IUser = {
       email: user.email,
-      full_name: user.email,
-      username: user.email,
       password: user.password,
+      name: user.email,
     };
 
     if (data.password) {
@@ -76,20 +58,13 @@ class UsersService {
       updateData.email = data.email;
     }
 
-    if (data.full_name) {
-      updateData.full_name = data.full_name;
+    if (data.name) {
+      updateData.name = data.name;
     }
 
-    if (data.username) {
-      updateData.username = data.username;
-    }
-
-    const updatedUser: IUserDocument | null = await this.usersModel.findByIdAndUpdate(
-      { ...updateData },
-      {
-        new: true,
-      },
-    );
+    const updatedUser: IUserDocument | null = await this.usersModel.findByIdAndUpdate(updateData, {
+      new: true,
+    });
 
     if (!updatedUser) {
       throw "Unable to update user info";
@@ -102,6 +77,12 @@ class UsersService {
     await user.remove();
 
     return "User deleted";
+  }
+
+  public async bulkRemove(data?: IUser): Promise<string> {
+    await this.usersModel.remove({ ...data });
+
+    return "Users removed";
   }
 }
 
